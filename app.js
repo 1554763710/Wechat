7eab2fddcf354ce56be3ef1ccdd4917fea34730a
Guo <1554763710@qq.com>
2019-03-19 20:49:51 +0800

    模块化

--- app.js
+++ app.js
@@ -1,57 +1,9 @@
 const express = require("express");
-const sha1 = require("sha1");
-const {getData , setObj , simplifyObj} = require("./tools/tools");
+const replay = require("./replay/index");
 
 const app = express();
 
-app.use( async (req,res) =>{
-    const {signature ,echostr ,timestamp ,nonce} = req.query;
-    const token = "weixingongzonghao_guo";
-    const shaText = sha1([token,timestamp,nonce].sort().join(""));
-
-    if(req.method === "GET"){
-        // 对接微信服务器
-        if(shaText === signature){
-            res.end(echostr);
-        }else{
-            res.end("error");
-        }
-    }else if(req.method === "POST"){
-        // 接收客户端信息/通过微信服务器接收
-        if(shaText !== signature){
-            res.end("error");
-            return;
-        }
-        // 得到客户发送的数据
-        const xmlData = await getData(req);
-        // 将 xml 数据转为 js 对象
-        let jsData = setObj(xmlData);
-        // js 对象简化
-        let newJsData = simplifyObj(jsData);
-        /* { ToUserName: 'gh_4f5a976449de',
-            FromUserName: 'o6HZg1TckBEEkr4Tu5amb2JHHTEk',
-            CreateTime: '1552993577',
-            MsgType: 'text',
-            Content: '11',
-            MsgId: '22233525050256383' } */
-        // 设置自动发送消息(文本)
-        let content = "哈哈哈哈哈";
-        if(newJsData.Content === "1"){
-           content = "嘻嘻嘻嘻嘻嘻";
-        }else if(newJsData.Content === "2"){
-            content = "呵呵呵呵呵";
-        }
-
-        const text = `<xml>
-        <ToUserName><![CDATA[${newJsData.FromUserName}]]></ToUserName>
-        <FromUserName><![CDATA[${newJsData.ToUserName}]]></FromUserName>
-        <CreateTime>${Date.now()}</CreateTime>
-        <MsgType><![CDATA[text]]></MsgType>
-        <Content><![CDATA[${content}]]></Content>
-        </xml>`;
-        res.send(text);
-    }
-});
+app.use(replay());
 app.listen(3000,err =>{
     if(!err) console.log("服务器启动成功");
     else console.log(err);
---
+++ replay/type.js
@@ -0,0 +1,46 @@
+
+module.exports =  (options) =>{
+    let text = `<xml>
+        <ToUserName><![CDATA[${options.toUserName}]]></ToUserName>
+        <FromUserName><![CDATA[${options.fromUserName}]]></FromUserName>
+        <CreateTime>${options.createTime}</CreateTime>
+        <MsgType><![CDATA[${options.type}]]></MsgType>`;
+    if(options.type === "text"){
+        text += `<Content><![CDATA[${options.content}]]></Content>`;
+    }else if(options.type === "image"){
+        text += `<Image>
+        <MediaId><![CDATA[${options.mediaid}]]></MediaId>
+      </Image>`;
+    }else if (options.type === 'voice') {
+        text += `<Voice>
+        <MediaId><![CDATA[${options.mediaId}]]></MediaId>
+      </Voice>`
+      } else if (options.type === 'video') {
+        text += `<Video>
+        <MediaId><![CDATA[${options.mediaId}]]></MediaId>
+        <Title><![CDATA[${options.title}]]></Title>
+        <Description><![CDATA[${options.description}]]></Description>
+      </Video>`
+      } else if (options.type === 'music') {
+        text += `<Music>
+        <Title><![CDATA[${options.title}]]></Title>
+        <Description><![CDATA[${options.description}]]></Description>
+        <MusicUrl><![CDATA[${options.musicUrl}]]></MusicUrl>
+        <HQMusicUrl><![CDATA[${options.hqMusicUrl}]]></HQMusicUrl>
+        <ThumbMediaId><![CDATA[${options.mediaId}]]></ThumbMediaId>
+      </Music>`
+      } else if (options.type === 'news'){
+        text += options.arr.reduce((pre , item)=>{
+            return pre + `<item>
+            <Title><![CDATA[${item.title}]]></Title>
+            <Description><![CDATA[${item.description}]]></Description>
+            <PicUrl><![CDATA[${item.picUrl}]]></PicUrl>
+            <Url><![CDATA[${item.url}]]></Url>
+          </item>`
+        } , `<ArticleCount>${options.arr.length}</ArticleCount>
+        <Articles>`)
+        text += `</Articles>`;
+      }
+    text += `</xml>`;
+    return text;
+}
