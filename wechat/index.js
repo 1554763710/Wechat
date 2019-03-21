64c095ba555d4715ddfecb94dfff069f7ba50515
Guo <1554763710@qq.com>
2019-03-21 16:57:04 +0800

    添加用户标签

--- wechat/index.js
+++ wechat/index.js
@@ -30,28 +30,102 @@ const menu =  {
            }]
       }]
 }
-
+const URL_PREFIX = "https://api.weixin.qq.com/cgi-bin/";
+// 创建菜单
 async function createMenu(){
     const { access_token } = await fetchAT();
-    const url = `https://api.weixin.qq.com/cgi-bin/menu/create?access_token=${access_token}`;
+    const url = `${URL_PREFIX}menu/create?access_token=${access_token}`;
 
     // 发送post请求
     const result = rq({method : "POST" ,url ,json : true ,body : menu}); 
     return result;
 }
-
+// 删除菜单
 async function deleteMenu(){
     const { access_token } = await fetchAT();
-    const url = `https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=${access_token}`;
+    const url = `${URL_PREFIX}menu/delete?access_token=${access_token}`;
 
     // 发送post请求
     const result = rq({method : "POST" ,url ,json : true });
     return result;
 }
+// 创建用户标签
+async function createTag(name){
+    const { access_token } = await fetchAT();
+    const url = `${URL_PREFIX}tags/create?access_token=${access_token}`;
 
+    // 发送post请求
+    const result = rq({method:"POST", url, json:true , body:{"tag":{ name }}});
+    return result;
+}
+// 批量为用户打下标签
+async function usersTag(openid_list, tagid){
+    const { access_token } = await fetchAT();
+    const url = `${URL_PREFIX}tags/members/batchtagging?access_token=${access_token}`;
+
+    // 发送post请求
+    const result = rq({method:"POST", url, json:true , body:{ openid_list, tagid }});
+    return result;
+}
+// 得到标签下的用户
+async function getTagUsers(tagid, next_openid = ""){
+    const { access_token } = await fetchAT();
+    const url = `${URL_PREFIX}user/tag/get?access_token=${access_token}`;
+
+    // 发送post请求
+    const result = rq({method:"POST", url, json:true , body:{ tagid, next_openid }});
+    return result;
+}
+// 得到所有的标签名
+async function getTags(){
+    const { access_token } = await fetchAT();
+    const url = `${URL_PREFIX}tags/get?access_token=${access_token}`;
+
+    // 发送post请求
+    const result = rq({method:"GET", url, json:true });
+    return result;
+}
+// 通过标签群发消息
+async function messageSendAll(body){
+    const { access_token } = await fetchAT();
+    const url = `${URL_PREFIX}message/mass/sendall?access_token=${access_token}`;
+
+    // 发送post请求
+    const result = rq({method:"POST", url, json:true ,body});
+    return result;
+}
+// 运行
 (async ()=>{
-    let result = await deleteMenu();
+
+    // 自定义菜单(要先将之前删除,在自定义菜单)
+    /*let result = await deleteMenu();
     console.log(result);
     result = await createMenu();
+    console.log(result);*/
+
+    // 用户分类 (创建标签 --> 添加用户 --> 获取标签下的用户)
+    // let result1 = await createTag("class");
+    // console.log(result1);
+    // let result2 = await usersTag(["o6HZg1TckBEEkr4Tu5amb2JHHTEk"], result1.tag.id);
+    // console.log(result2);
+    // let result3 = await getTagUsers(result1.tag.id);
+    // console.log(result3);
+
+    // 得到标签名
+    // let result = await getTags();
+    // console.log(result);
+
+    // 通过标签群发消息
+    const body = {
+        "filter":{
+           "is_to_all":true,
+           "tag_id":100
+        },
+        "text":{
+           "content":"大吉大利"
+        },
+         "msgtype":"text"
+    }
+    let result = await messageSendAll(body);
     console.log(result);
 })();
---
+++ wechat/port.js
@@ -0,0 +1,10 @@
+const fetchAT = require("./getAT");
+const rq = require("request-promise-native");
+
+async function fn(method, url, body = {}){
+    if(url.indexOf("tags") !== -1 ){
+        if(url.indexOf("get") !== -1){
+            
+        }
+    }
+}
